5736675371763ab62817433a8f1663aac398db34
Keanu Reaño <reanokeanu@gmail.com>
2024-10-21 13:48:27 +0800

    add feature games component displaying mock games.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Banner from "@/components/Banner";
+import Games from "@/components/Games";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="container min-h-screen p-2 text-primary">
       <Navbar />
       <Banner />
+      <Games />
     </main>
   );
 }
---
+++ components/Games.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+const mockGames: Game[] = [
+  { id: 1, name: "Game 1" },
+  { id: 2, name: "Game 2" },
+  { id: 3, name: "Game 3" },
+];
+
+export default function Games() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      const response = await new Promise<Game[]>((resolve) => {
+        setTimeout(() => {
+          resolve(mockGames);
+        }, 1000);
+      });
+
+      setGames(response);
+    };
+    fetchGames();
+  }, []);
+  return (
+    <div>
+      {games.map((game) => (
+        <div>{game.name}</div>
+      ))}
+    </div>
+  );
+}
