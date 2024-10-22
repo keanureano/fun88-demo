5f9aae45f6fa88ea8e82ffd2f5457866eb2d8d78
Keanu Reaño <reanokeanu@gmail.com>
2024-10-22 10:46:45 +0800

    update games categories and games list ui, add card component

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="container min-h-screen p-2 text-primary">
+    <main className="max-w-xl min-h-[101dvh] p-2 mx-auto text-primary">
       <Navbar />
       <Banner />
       <Games />
--- components/Games.tsx
+++ components/Games.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "./ui/tabs";
 import { Input } from "./ui/input";
+import Image from "next/image";
+import { Button } from "./ui/button";
 
 interface Game {
   id: number;
@@ -52,122 +54,151 @@ const mockGames: Game[] = [
     id: 1,
     name: "Shaolin Crew",
     img: "./games/shaolin-crew.png",
-    categories: ["live", "new"],
+    categories: ["start", "live", "new"],
     gameProvider: "Xpans",
   },
   {
     id: 2,
     name: "Big Bad Wolf",
     img: "./games/big-bad-wolf.png",
-    categories: ["jackpots", "start"],
+    categories: ["start", "jackpots"],
     gameProvider: "Playtech",
   },
   {
     id: 3,
     name: "Book of Egypt",
     img: "./games/book-of-egypt.png",
-    categories: ["live", "jackpots"],
+    categories: ["start", "live", "jackpots"],
     gameProvider: "Xpans",
   },
   {
     id: 4,
     name: "Pirates Power",
     img: "./games/pirates-power.png",
-    categories: ["new", "start"],
+    categories: ["start", "new"],
     gameProvider: "Xpans",
   },
   {
     id: 5,
-    name: "Crocodile Blitz Extreme FB",
-    img: "./games/crocodile-blitz-extreme-fb.png",
+    name: "Crocodile Blitz Xtreme FB",
+    img: "./games/crocodile-blitz-xtreme-fb.png",
     categories: ["start", "new", "live"],
     gameProvider: "Playtech",
   },
   {
     id: 6,
-    name: "Anaconda Wild 1 Powerplay Jackpot",
-    img: "./games/anaconda-wild-3-powerplay-jackpot.png",
-    categories: ["live"],
+    name: "Anaconda Wild 2 Powerplay Jackpot",
+    img: "./games/anaconda-wild-2-powerplay-jackpot.png",
+    categories: ["start", "live"],
     gameProvider: "Playtech",
   },
   {
     id: 7,
     name: "Maya Jackpot",
     img: "./games/maya-jackpot.png",
-    categories: ["jackpots"],
+    categories: ["start", "jackpots"],
     gameProvider: "Skywind",
   },
   {
     id: 8,
     name: "Beach Life",
     img: "./games/beach-life.png",
-    categories: ["jackpots", "start"],
+    categories: ["start", "jackpots"],
     gameProvider: "Playtech",
   },
   {
     id: 9,
     name: "Inca Jackpot",
     img: "./games/inca-jackpot.png",
-    categories: ["jackpots", "new"],
+    categories: ["start", "jackpots", "new"],
     gameProvider: "Skywind",
   },
   {
     id: 10,
     name: "Pride of Persia Empire Treasures",
     img: "./games/pride-of-persia-empire-treasures.png",
-    categories: ["new"],
+    categories: ["start", "new"],
     gameProvider: "Playtech",
   },
   {
     id: 11,
     name: "Azteca Bonus Lines Powerplay Jackpot",
     img: "./games/azteca-bonus-lines-powerplay-jackpot.png",
-    categories: ["live", "start"],
+    categories: ["start", "live"],
     gameProvider: "Playtech",
   },
 ];
 
 export default function Games() {
-  const [games, setGames] = useState<Game[]>([]);
+  const [filteredGames, setFilteredGames] = useState<Game[]>([]);
   const [activeCategory, setActiveCategory] = useState("start");
   const [searchQuery, setSearchQuery] = useState("");
 
-  const filteredGames = useMemo(() => {
+  const fetchFilteredGames = async () => {
+    setFilteredGames([]);
+
+    const response = await new Promise<Game[]>((resolve) => {
+      setTimeout(() => {
+        resolve(mockGames);
+      }, 1000);
+    });
+
     if (activeCategory === "search") {
       if (searchQuery === "") {
-        return games;
+        setFilteredGames(response);
+      } else {
+        setFilteredGames(
+          response.filter((game) =>
+            game.name.toLowerCase().includes(searchQuery.toLowerCase())
+          )
+        );
       }
-      return games.filter((game) =>
-        game.name.toLowerCase().includes(searchQuery.toLowerCase())
+    } else {
+      setFilteredGames(
+        response.filter((game) => game.categories.includes(activeCategory))
       );
     }
-
-    return games.filter((game) => game.categories.includes(activeCategory));
-  }, [games, searchQuery, activeCategory]);
+  };
 
   useEffect(() => {
-    const getAllGames = async () => {
-      const response = await new Promise<Game[]>((resolve) => {
-        setTimeout(() => {
-          resolve(mockGames);
-        }, 999);
-      });
-
-      setGames(response);
-    };
-    getAllGames();
-  }, []);
+    fetchFilteredGames();
+  }, [searchQuery, activeCategory]);
 
   return (
     <Tabs defaultValue="start" onValueChange={setActiveCategory}>
-      <TabsList>
-        {mockCategories.map((category) => (
-          <TabsTrigger key={category.id} value={category.name}>
-            {category.name.charAt(0).toUpperCase() + category.name.slice(1)}
-          </TabsTrigger>
-        ))}
-      </TabsList>
+      <GamesCategories />
+      <GamesContent
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        filteredGames={filteredGames}
+      />
+    </Tabs>
+  );
+}
 
+function GamesCategories() {
+  return (
+    <TabsList className="w-full gap-10">
+      {mockCategories.map((category) => (
+        <TabsTrigger key={category.id} value={category.name}>
+          {category.name.charAt(0).toUpperCase() + category.name.slice(1)}
+        </TabsTrigger>
+      ))}
+    </TabsList>
+  );
+}
+
+function GamesContent({
+  searchQuery,
+  setSearchQuery,
+  filteredGames,
+}: {
+  searchQuery: string;
+  setSearchQuery: Dispatch<SetStateAction<string>>;
+  filteredGames: Game[];
+}) {
+  return (
+    <>
       {mockCategories.map((category) => (
         <TabsContent key={category.id} value={category.name}>
           {category.name === "search" && (
@@ -175,17 +206,34 @@ export default function Games() {
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
               placeholder="Search games"
+              className="my-2"
             />
           )}
-          <div>
-            {filteredGames.map((game) => (
-              <div>
-                <h1>{game.name}</h1>
-              </div>
-            ))}
-          </div>
+          <GamesList filteredGames={filteredGames} />
         </TabsContent>
       ))}
-    </Tabs>
+    </>
+  );
+}
+
+function GamesList({ filteredGames }: { filteredGames: Game[] }) {
+  return (
+    <div className="grid grid-cols-3 gap-2">
+      {filteredGames.map((game) => (
+        <Button
+          variant="ghost"
+          className="w-full h-full p-0 m-0 transition hover:brightness-110 overflow-hidden rounded-3xl"
+          key={game.id}
+        >
+          <Image
+            src={game.img}
+            alt={game.name}
+            width={200}
+            height={200}
+            className="object-cover w-full h-full hover:scale-105 transition"
+          />
+        </Button>
+      ))}
+    </div>
   );
 }
