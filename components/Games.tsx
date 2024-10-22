85bd7ce69d05b9895d363ad518a78588114c2860
Keanu Reaño <reanokeanu@gmail.com>
2024-10-22 13:17:42 +0800

    update games component to add game provider filters

--- components/Games.tsx
+++ components/Games.tsx
@@ -15,54 +15,48 @@ import {
 } from "lucide-react";
 import clsx from "clsx";
 import { toast } from "@/hooks/use-toast";
+import { Card, CardContent, CardDescription, CardHeader } from "./ui/card";
+import { Button } from "./ui/button";
 
 interface Game {
-  id: number;
   name: string;
   img: string;
   categories: string[];
   gameProvider: string;
+  gameProviderImg: string;
 }
 
 interface Category {
-  id: number;
   name: string;
   icon: React.ReactNode;
 }
 
 const mockCategories: Category[] = [
   {
-    id: 1,
     name: "search",
     icon: <Search />,
   },
   {
-    id: 2,
     name: "start",
     icon: <Flame />,
   },
   {
-    id: 3,
     name: "new",
     icon: <Gamepad2 />,
   },
   {
-    id: 4,
     name: "slots",
     icon: <Joystick />,
   },
   {
-    id: 5,
     name: "live",
     icon: <VenetianMask />,
   },
   {
-    id: 6,
     name: "jackpots",
     icon: <CircleDollarSign />,
   },
   {
-    id: 7,
     name: "favorites",
     icon: <Star />,
   },
@@ -70,88 +64,88 @@ const mockCategories: Category[] = [
 
 const mockGames: Game[] = [
   {
-    id: 0,
     name: "Sugar Rush",
     img: "./games/sugar-rush.png",
     categories: ["start", "new"],
     gameProvider: "PragmaticPlay",
+    gameProviderImg: "./providers/pragmaticplay.png",
   },
   {
-    id: 1,
     name: "Shaolin Crew",
     img: "./games/shaolin-crew.png",
     categories: ["start", "live", "new"],
     gameProvider: "Xpans",
+    gameProviderImg: "./providers/xpans.png",
   },
   {
-    id: 2,
     name: "Big Bad Wolf",
     img: "./games/big-bad-wolf.png",
     categories: ["start", "jackpots"],
     gameProvider: "Playtech",
+    gameProviderImg: "./providers/playtech.png",
   },
   {
-    id: 3,
     name: "Book of Egypt",
     img: "./games/book-of-egypt.png",
     categories: ["start", "live", "jackpots"],
     gameProvider: "Xpans",
+    gameProviderImg: "./providers/xpans.png",
   },
   {
-    id: 4,
     name: "Pirates Power",
     img: "./games/pirates-power.png",
     categories: ["start", "new"],
     gameProvider: "Xpans",
+    gameProviderImg: "./providers/xpans.png",
   },
   {
-    id: 5,
     name: "Crocodile Blitz Xtreme FB",
     img: "./games/crocodile-blitz-xtreme-fb.png",
     categories: ["start", "new", "live"],
     gameProvider: "Playtech",
+    gameProviderImg: "./providers/playtech.png",
   },
   {
-    id: 6,
     name: "Anaconda Wild 2 Powerplay Jackpot",
     img: "./games/anaconda-wild-2-powerplay-jackpot.png",
     categories: ["start", "live"],
     gameProvider: "Playtech",
+    gameProviderImg: "./providers/playtech.png",
   },
   {
-    id: 7,
     name: "Maya Jackpot",
     img: "./games/maya-jackpot.png",
     categories: ["start", "jackpots"],
     gameProvider: "Skywind",
+    gameProviderImg: "./providers/skywind.png",
   },
   {
-    id: 8,
     name: "Beach Life",
     img: "./games/beach-life.png",
     categories: ["start", "jackpots"],
     gameProvider: "Playtech",
+    gameProviderImg: "./providers/playtech.png",
   },
   {
-    id: 9,
     name: "Inca Jackpot",
     img: "./games/inca-jackpot.png",
     categories: ["start", "jackpots", "new"],
     gameProvider: "Skywind",
+    gameProviderImg: "./providers/skywind.png",
   },
   {
-    id: 10,
     name: "Pride of Persia Empire Treasures",
     img: "./games/pride-of-persia-empire-treasures.png",
     categories: ["start", "new"],
     gameProvider: "Playtech",
+    gameProviderImg: "./providers/playtech.png",
   },
   {
-    id: 11,
     name: "Azteca Bonus Lines Powerplay Jackpot",
     img: "./games/azteca-bonus-lines-powerplay-jackpot.png",
     categories: ["start", "live"],
     gameProvider: "Playtech",
+    gameProviderImg: "./providers/playtech.png",
   },
 ];
 
@@ -159,35 +153,44 @@ export default function Games() {
   const [games, setGames] = useState<Game[]>(mockGames);
   const [filteredGames, setFilteredGames] = useState<Game[]>([]);
   const [activeCategory, setActiveCategory] = useState("start");
+  const [activeGameProvider, setActiveGameProvider] = useState("all");
   const [searchQuery, setSearchQuery] = useState("");
 
   const fetchFilteredGames = async () => {
     const response = await new Promise<Game[]>((resolve) => {
       setTimeout(() => {
-        resolve(games);
+        if (activeGameProvider === "all") {
+          resolve(games);
+          return;
+        }
+        resolve(
+          games.filter((game) => game.gameProvider === activeGameProvider)
+        );
+        return;
       }, 500);
     });
 
     if (activeCategory === "search") {
       if (searchQuery === "") {
         setFilteredGames(response);
-      } else {
-        setFilteredGames(
-          response.filter((game) =>
-            game.name.toLowerCase().includes(searchQuery.toLowerCase())
-          )
-        );
+        return;
       }
-    } else {
       setFilteredGames(
-        response.filter((game) => game.categories.includes(activeCategory))
+        response.filter((game) =>
+          game.name.toLowerCase().includes(searchQuery.toLowerCase())
+        )
       );
+      return;
     }
+
+    setFilteredGames(
+      response.filter((game) => game.categories.includes(activeCategory))
+    );
   };
 
   useEffect(() => {
     fetchFilteredGames();
-  }, [searchQuery, activeCategory, games]);
+  }, [searchQuery, activeCategory, games, activeGameProvider]);
 
   return (
     <Tabs defaultValue="start" onValueChange={setActiveCategory}>
@@ -198,6 +201,8 @@ export default function Games() {
         filteredGames={filteredGames}
         games={games}
         setGames={setGames}
+        activeGameProvider={activeGameProvider}
+        setActiveGameProvider={setActiveGameProvider}
       />
     </Tabs>
   );
@@ -208,7 +213,7 @@ function GamesCategories() {
     <TabsList className="flex w-full h-full justify-evenly">
       {mockCategories.map((category) => (
         <TabsTrigger
-          key={category.id}
+          key={category.name}
           value={category.name}
           className="flex flex-col flex-1 px-0"
         >
@@ -228,17 +233,21 @@ function GamesContent({
   filteredGames,
   games,
   setGames,
+  activeGameProvider,
+  setActiveGameProvider,
 }: {
   searchQuery: string;
   setSearchQuery: Dispatch<SetStateAction<string>>;
   filteredGames: Game[];
   games: Game[];
   setGames: Dispatch<SetStateAction<Game[]>>;
+  activeGameProvider: string;
+  setActiveGameProvider: Dispatch<SetStateAction<string>>;
 }) {
   return (
     <>
       {mockCategories.map((category) => (
-        <TabsContent key={category.id} value={category.name}>
+        <TabsContent key={category.name} value={category.name}>
           {category.name === "search" && (
             <Input
               value={searchQuery}
@@ -254,6 +263,11 @@ function GamesContent({
           />
         </TabsContent>
       ))}
+      <GamesProviders
+        games={games}
+        activeGameProvider={activeGameProvider}
+        setActiveGameProvider={setActiveGameProvider}
+      />
     </>
   );
 }
@@ -270,7 +284,7 @@ function GamesList({
   const handleFavoriteClick = (currentGame: Game) => {
     setGames(
       games.map((game) => {
-        if (currentGame.id === game.id) {
+        if (currentGame.name === game.name) {
           const isFavorited = game.categories.includes("favorites");
           toast({
             variant: isFavorited ? "destructive" : "primary",
@@ -295,7 +309,7 @@ function GamesList({
       {filteredGames.map((game) => (
         <div
           className="relative w-full h-full p-0 m-0 overflow-hidden transition cursor-pointer hover:brightness-110 rounded-3xl"
-          key={game.id}
+          key={game.name}
         >
           <Image
             src={game.img}
@@ -318,6 +332,74 @@ function GamesList({
           />
         </div>
       ))}
+    </div>
+  );
+}
+
+function GamesProviders({
+  games,
+  activeGameProvider,
+  setActiveGameProvider,
+}: {
+  games: Game[];
+  activeGameProvider: string;
+  setActiveGameProvider: Dispatch<SetStateAction<string>>;
+}) {
+  const providers: { name: string; img: string }[] = Array.from(
+    new Set(
+      games.map((game) =>
+        JSON.stringify({
+          name: game.gameProvider,
+          img: game.gameProviderImg,
+        })
+      )
+    )
+  ).map((provider) => JSON.parse(provider));
+
+  const handleProviderClick = (providerName: string) => {
+    setActiveGameProvider(providerName);
+  };
+
+  return (
+    <div className="pt-4 pb-16">
+      <h2 className="pb-2 text-sm text-card-foreground">
+        Game Providers
+        {activeGameProvider !== "all" && (
+          <Button
+            variant="link"
+            className="px-2"
+            onClick={() => setActiveGameProvider("all")}
+          >
+            Clear Filter
+          </Button>
+        )}
+      </h2>
+      <div className="grid grid-cols-3 gap-2">
+        {providers.map((provider) => (
+          <Card
+            key={provider.name}
+            className={clsx(
+              "flex flex-col items-center transition bg-neutral-200 cursor-pointer hover:brightness-105",
+              {
+                "border-primary border-2": provider.name === activeGameProvider,
+              }
+            )}
+            onClick={() => handleProviderClick(provider.name)}
+          >
+            <CardHeader className="flex items-center justify-center flex-1">
+              <Image
+                src={provider.img}
+                alt={provider.name}
+                width={100}
+                height={100}
+              />
+            </CardHeader>
+            <CardContent className="w-full py-4 text-center bg-neutral-100/75 rounded-b-lg">
+              <CardDescription>{provider.name}</CardDescription>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,13 +4,15 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between pb-4">
       <div className="flex items-center gap-2">
-        <Menu className="stroke-primary" />
-        <h1 className="text-2xl italic font-black">FUN88</h1>
+        <Menu className="stroke-primary cursor-pointer hover:brightness-75 transition" />
+        <h1 className="text-2xl italic font-black cursor-pointer hover:brightness-75 transition">
+          FUN88
+        </h1>
       </div>
       <div className="flex items-center gap-2">
-        <Wallet className="stroke-muted-foreground" />
+        <Wallet className="stroke-muted-foreground cursor-pointer hover:brightness-75 transition" />
         <h2 className="font-semibold">$1990.6 | </h2>
-        <User className="stroke-primary fill-primary" />
+        <User className="stroke-primary fill-primary cursor-pointer hover:brightness-75 transition" />
       </div>
     </nav>
   );
